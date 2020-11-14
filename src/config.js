6df05c006fa7a5dc8f0ea7e44bcab6e0d7d72030
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-11-14 20:41:08 +0100

    Add index.html

--- src/config.js
+++ src/config.js
@@ -1,7 +1,15 @@
 // PAT with no scopes. Readonly to public information on github
 const PAT = "64c0ffde304a67cd3cab32f23290440d754b52c2";
 export const FetchConfig = {
-    headers: {
-        Authorization: `token ${PAT}`,
-    },
+  headers: {
+    Authorization: `token ${PAT}`,
+    "Content-Type": "application/json",
+  },
+};
+
+export const GqlConfig = {
+  headers: {
+    Authorization: `bearer ${PAT}`,
+    "Content-Type": "application/json",
+  },
 };
--- src/metadata.js
+++ src/metadata.js
@@ -1,45 +1,112 @@
-import { FetchConfig } from "./config.js";
+import { GqlConfig } from "./config.js";
 import fetch from "node-fetch";
 import repos from "./db/flathub/repos.js";
-import { writeFile } from "fs/promises";
-let metadatas = [];
+import { appendFile, unlink } from "fs/promises";
 let index = 0;
 
-for (const { name, full_name, default_branch } of repos) {
-  let wayland = false;
-  let x11 = false;
-  let filesystem = false;
-  let filename = `https://raw.githubusercontent.com/${full_name}/${default_branch}/${name}.json`;
-  let found = false;
-  const json = await fetch(filename, FetchConfig);
+const MEATDATA_JSON = "./src/db/flathub/metadata.js";
 
-  if (json.ok) {
-    /** json */
-    const jsonStr = await json.text();
-    found = true;
-    wayland = !!jsonStr.match(/--socket=wayland/);
-    x11 = !!jsonStr.match(/--socket=x11/);
-    filesystem = !!jsonStr.match(/--filesystem=/);
-  } else {
-    /** yaml */
-    filename = `https://raw.githubusercontent.com/${full_name}/${default_branch}/${name}.yaml`;
-    const yaml = await fetch(filename, FetchConfig);
-    if (yaml.ok) {
-      const yamlStr = await yaml.text();
-      found = true;
-      wayland = !!yamlStr.match(/--socket=wayland/);
-      x11 = !!yamlStr.match(/--socket=x11/);
-      filesystem = !!yamlStr.match(/--filesystem=/);
+await unlink(MEATDATA_JSON);
+await appendFile(MEATDATA_JSON, "export default [");
+await makeMetadata(repos);
+await appendFile(MEATDATA_JSON, "]");
+
+async function makeMetadata(repos) {
+  for (const { name } of repos) {
+    const json = await makeGqlQuery(name, "json");
+    let ext = null;
+    let history = null;
+
+    if (json) {
+      history = parseHistory(json);
+      ext = "json";
+    } else {
+      const yaml = await makeGqlQuery(name, "yaml");
+
+      if (yaml) {
+        history = parseHistory(yaml);
+        ext = "yaml";
+      }
     }
-  }
 
-  const metadata = { index, x11, wayland, filesystem, found, filename };
-  console.log({ metadata });
-  index += 1;
-  metadatas.push(metadata);
+    const metadata = { index, name, ext, history };
+    console.log(index);
+
+    await appendFile(MEATDATA_JSON, `${JSON.stringify(metadata, null, 2)},`);
+    index += 1;
+  }
 }
 
-await writeFile(
-  `./src/db/flathub/metadata.js`,
-  `export default ${JSON.stringify(metadatas, null, 2)}`
-);
+function parseHistory(history) {
+  const transformed = history.map(({ date, text }) => {
+    const x11 = !!text?.match(/--socket=x11/);
+    const wayland = !!text?.match(/--socket=wayland/);
+    const filesystemHome = !!text?.match(/--filesystem=home/);
+    const filesystemHost = !!text?.match(/--filesystem=host/);
+    const filesystem = !!text?.match(/--filesystem=/);
+
+    return { date, x11, wayland, filesystem, filesystemHome, filesystemHost };
+  });
+
+  return transformed;
+}
+
+async function makeGqlQuery(filename, ext) {
+  const query = `\
+query {\
+    repository(name:"${filename}", owner:"flathub") {\
+      name\
+      defaultBranchRef {\
+        target {\
+          ... on Commit {\
+            history(since: "2018-01-01T00:00:00Z", path:"${filename}.${ext}") {\
+              nodes {\
+                committedDate\
+                file(path:"${filename}.${ext}") {\
+                  object {\
+                    ... on Blob {\
+                      text\
+                    }\
+                  }\
+                }\
+              }\
+            }\
+          }\
+        }\
+      }\
+    }\
+  }\
+`;
+  const body = JSON.stringify({ query });
+  const res = await fetch("https://api.github.com/graphql", {
+    ...GqlConfig,
+    method: "POST",
+    body,
+  });
+
+  const json = await res.json();
+
+  if (!json.data?.repository?.defaultBranchRef) {
+    console.log(JSON.stringify(json, null, 2));
+    return null;
+  }
+
+  const {
+    data: {
+      repository,
+      repository: {
+        name,
+        defaultBranchRef: {
+          target: {
+            history: { nodes },
+          },
+        },
+      },
+    },
+  } = json;
+
+  return nodes.map(({ committedDate, file }) => ({
+    date: committedDate,
+    text: file?.object?.text ?? null,
+  }));
+}
