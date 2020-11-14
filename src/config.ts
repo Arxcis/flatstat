44b0063192533bd5e8f691217d3a56b41db301e7
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-11-14 15:51:46 +0100

    un tsc project

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-const PAT = "64c0ffde304a67cd3cab32f23290440d754b52c2";
-export const FetchConfig = {
-  headers: {
-    Authorization: `token ${PAT}`,
-  },
-};
--- src/count.js
+++ src/count.js
@@ -1,14 +1,23 @@
 import repos from "./db/flathub/repos.js";
 import metadata from "./db/flathub/metadata.js";
 import { writeFile } from "fs/promises";
+
 const count = {
-    repos: repos.reduce((acc) => acc + 1, 0),
-    wayland: metadata.reduce((acc, it) => acc + it.socketWayland, 0),
-    x11: metadata.reduce((acc, it) => acc + it.socketX11, 0),
-    onlyX11: metadata.reduce((acc, it) => acc + (it.socketX11 && !it.socketWayland), 0),
-    onlyWayland: metadata.reduce((acc, it) => acc + (!it.socketX11 && it.socketWayland), 0),
-    bothX11AndWayland: metadata.reduce((acc, it) => acc + (it.socketX11 && it.socketWayland), 0),
-    noGUI: metadata.reduce((acc, it) => acc + (!it.socketX11 && !it.socketWayland), 0),
+  repos: repos.reduce((acc) => acc + 1, 0),
+  x11: metadata.reduce((acc, it) => acc + it.x11, 0),
+  wayland: metadata.reduce((acc, it) => acc + it.wayland, 0),
+  filesystem: metadata.reduce((acc, it) => acc + it.filesystem, 0),
+  notFound: metadata.reduce((acc, it) => acc + !it.found, 0),
+  onlyX11: metadata.reduce((acc, it) => acc + (it.x11 && !it.wayland), 0),
+  onlyWayland: metadata.reduce((acc, it) => acc + (!it.x11 && it.wayland), 0),
+  bothX11AndWayland: metadata.reduce(
+    (acc, it) => acc + (it.x11 && it.wayland),
+    0
+  ),
+  noGUI: metadata.reduce((acc, it) => acc + (!it.x11 && !it.wayland), 0),
 };
 console.log({ count });
-await writeFile("./src/db/flathub/count.js", `export default ${JSON.stringify(count, null, 2)}`);
+await writeFile(
+  "./src/db/flathub/count.js",
+  `export default ${JSON.stringify(count, null, 2)}`
+);
