d6cb7e146a3513a9d22ff0255f371d6ef02a042d
Jonas Johan Solsvik <jonas@maritimeoptima.com>
2020-11-16 13:51:41 +0100

    Fix issue #4 by storing only 1 item per month in metafile-history

--- db/flathub/metafiles.js
+++ db/flathub/metafiles.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d32e26c4cf5bdb814d003a19606de44db2e7f1e072c3c262c8a4e208c325e3f
-size 7241230
+oid sha256:b021f6982c5051e060a6d66ae58c672e015ec12153165bd6d497bddd369da773
+size 24673
