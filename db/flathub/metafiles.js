451712f1b5a69618274c8eb9c5ba5f3aeda2c8b6
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-11-16 10:08:42 +0100

    Run make metafiles

--- db/flathub/metafiles.js
+++ db/flathub/metafiles.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a5e16f06b5aca541c52e1f573639b7b03a38a253a58c7752a557d85e522136a
-size 7227416
+oid sha256:0d32e26c4cf5bdb814d003a19606de44db2e7f1e072c3c262c8a4e208c325e3f
+size 7241230
