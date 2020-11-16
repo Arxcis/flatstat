050d8e733b12f430f509fce077a9f1fd6536fbb7
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-11-16 09:35:35 +0100

    Refactor count.js

--- db/flathub/count.js
+++ db/flathub/count.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:429b2d1affed56d0075e87bc9ba9f39407810391d0fd89353c61d74220f6c440
+oid sha256:9dda4c29c3a89c3a106377f7dcda531b6142a8b827692c17363cc1d3053c1f2d
 size 16711
