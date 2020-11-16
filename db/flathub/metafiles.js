dbf60690e2a3dba46c122a9857503dbf8f3b96f7
Jonas Johan Solsvik <jonas@maritimeoptima.com>
2020-11-16 14:15:03 +0100

    Run make metafiles

--- db/flathub/metafiles.js
+++ db/flathub/metafiles.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b021f6982c5051e060a6d66ae58c672e015ec12153165bd6d497bddd369da773
-size 24673
+oid sha256:7baa71e64ab2e77c7192c4015a8c7923f687dcb4bd226765740ae5f9cb2ffe2f
+size 2820970
