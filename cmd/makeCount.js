3ef755da682cc116bf4b993f3c1c731cbfd35f1a
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-11-18 20:19:47 +0100

    Count finishArgs

--- cmd/makeCount.js
+++ cmd/makeCount.js
@@ -1,6 +1,6 @@
 import { writeFile, readFile } from "fs/promises";
 import { MONTHS } from "./config.js";
-import { incrementAchievementCount, incrementHoleCount, incrementPortalCount } from "../lib/stats.js";
+import { incrementAchievementCount, incrementHoleCount, incrementPortalCount, incrementFinishArgs } from "../lib/stats.js";
 
 const metafiles = JSON.parse(await readFile("./data/flathub/metafiles.json"));
 
@@ -39,18 +39,38 @@ const countMap = metafiles.reduce((acc, it) => {
 
       ...incrementAchievementCount(finishArgs, monthCount),
       ...incrementHoleCount(finishArgs, monthCount),
-      portals: incrementPortalCount(finishArgs, monthCount?.portals)
+      portals: incrementPortalCount(finishArgs, monthCount?.portals),
+      finishArgs: incrementFinishArgs(finishArgs, monthCount?.finishArgs)
     });
   }
+
 
   return acc;
 }, new Map());
 
 const count = [...countMap.entries()]
-  .map(([key, value]) => ({
-    month: key,
-    ...value,
-  }))
+  .map(([key, value]) => {
+
+    // Sort and filter away things that only happens once
+    const finishArgs = Object
+      .entries(value.finishArgs)
+      .filter(([, value]) => value > 1)
+      .sort(([, a], [, b]) => b - a)
+      .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+    const portals = Object
+      .entries(value.portals)
+      .filter(([, value]) => value > 1)
+      .sort(([, a], [, b]) => b - a)
+      .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+    return ({
+      month: key,
+      ...value,
+      finishArgs,
+      portals,
+    })
+  })
   .sort((a, b) => a.month.localeCompare(b.month));
 
 await writeFile(COUNT_JSON, JSON.stringify(count, null, 2));
