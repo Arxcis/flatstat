0b5ad6a62bf9bfff7a321dc29d57f64b6d277a7d
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-11-19 18:59:26 +0100

    Group finish-args with one occurance
    - if they starsWith() finish arg with more than one occurance.
    - Group them into an <other> group

--- cmd/makeCount.js
+++ cmd/makeCount.js
@@ -51,10 +51,35 @@ const countMap = metafiles.reduce((acc, it) => {
 const count = [...countMap.entries()]
   .map(([key, value]) => {
 
-    // Sort and filter away things that only happens once
-    const finishArgs = Object
+    // First get all finish args which happen more than once.
+    const moreFinishArgs = Object
       .entries(value.finishArgs)
-      .filter(([, value]) => value > 1)
+      .filter(([, value]) => value > 1);
+
+    // See if any of the args used only once, startswith args which happen more than once
+    const onceFinishArgs = Object
+      .entries(value.finishArgs)
+      .filter(([, value]) => value === 1);
+
+    const sortedMoreFinishArgs = moreFinishArgs
+      .sort(([, a], [, b]) => b - a)
+
+    const sortedByArgLength = [...moreFinishArgs].sort(([arga], [argb]) => argb.length - arga.length)
+
+    const combinedOnceFinishArgs = Object.entries(
+      onceFinishArgs.reduce((acc, [onceArg]) => {
+        const moreArg = sortedByArgLength.find(([moreArg]) => onceArg.startsWith(moreArg));
+        if (moreArg) {
+          return { ...acc, [`${moreArg[0]}&lt;other&gt;`]: (acc[`${moreArg[0]}&lt;other&gt;`] ?? 0) + 1 }
+        } else {
+          return acc
+        }
+      }, {})).filter(([, v]) => v > 1)
+
+    const finishArgs = [
+      ...sortedMoreFinishArgs,
+      ...combinedOnceFinishArgs
+    ]
       .sort(([, a], [, b]) => b - a)
       .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
 
