a06392ddc2968bca918e1bcc6f194ade50f5523e
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-07-07 15:26:15 +0200

    config.js / generate MONTHS based on start and end date

--- cmd/config.js
+++ cmd/config.js
@@ -1,4 +1,7 @@
-const PAT = process.env.GITHUB_PERSONAL_ACCESS_TOKEN ?? "<missing an env.GITHUB_PERSONAL_ACCESS_TOKEN>";
+const PAT =
+  process.env.GITHUB_PERSONAL_ACCESS_TOKEN ??
+  "<missing an env.GITHUB_PERSONAL_ACCESS_TOKEN>";
+
 export const RestConfig = {
   headers: {
     Authorization: `token ${PAT}`,
@@ -13,42 +16,13 @@ export const GqlConfig = {
   },
 };
 
-export const MONTHS = [
-  "2020-11",
-  "2020-12",
-  "2021-01",
-  "2021-02",
-  "2021-03",
-  "2021-04",
-  "2021-05",
-  "2021-06",
-  "2021-07",
-  "2021-08",
-  "2021-09",
-  "2021-10",
-  "2021-11",
-  "2021-12",
-  "2022-01",
-  "2022-02",
-  "2022-03",
-  "2022-04",
-  "2022-05",
-  "2022-06",
-  "2022-07",
-  "2022-08",
-  "2022-09",
-  "2022-10",
-  "2022-11",
-  "2022-12",
-  "2023-01",
-  "2023-02",
-  "2023-03",
-  "2023-04",
-  "2023-05",
-  "2023-06",
-  "2023-07",
-  "2023-08",
-  "2023-09",
-  "2023-10",
-  "2023-11",
-];
+const start = new Date("2018-01");
+const end = new Date("2025-07");
+export const MONTHS = [];
+
+while (start <= end) {
+  const month = start.toISOString().slice(0, 7);
+  MONTHS.push(month);
+  start.setMonth(start.getMonth() + 1);
+}
+console.log({ MONTHS });
