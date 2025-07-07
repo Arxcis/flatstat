564d21b2141d91839f68f962de6c0f3c3162c88c
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-07-07 15:33:35 +0200

    make-metafiles.js - introduce caching system

--- cmd/make-metafiles.js
+++ cmd/make-metafiles.js
@@ -1,36 +1,88 @@
-import { appendFile, unlink, readFile } from "fs/promises";
+import { readFile, writeFile, access, mkdir } from "fs/promises";
 import { queryMetafileHistory } from "./query.js";
 
-const apps = JSON.parse(await readFile("./data/flathub/apps.json"));
+(async function main() {
+  const CACHE_DIR = "./data/flathub/cache";
+  const APPS_FILE = "./data/flathub/apps.json";
+  const METAFILES_FILE = "./data/flathub/metafiles.json";
 
-const MEATFILES_JSON = "./data/flathub/metafiles.json";
+  const apps = JSON.parse(await readFile(APPS_FILE));
 
-await unlink(MEATFILES_JSON)
-await appendFile(MEATFILES_JSON, "[");
-await makeMetafiles(apps);
-await appendFile(MEATFILES_JSON, "]");
+  // 1. Make cache dir if not exist
+  await mkdir(CACHE_DIR, { recursive: true });
 
-async function makeMetafiles(apps) {
+  // 2. Cache one file for each app
+  const cachePaths = await makeMetafiles(CACHE_DIR, apps);
+
+  // 3. Combine files into one single file
+  await combineFiles(cachePaths, METAFILES_FILE);
+})();
+
+/** Cache one metafile for each app, and return paths to all cached files */
+async function makeMetafiles(CACHE_DIR, apps) {
   let index = 0;
+
+  const paths = [];
 
   for (const appID of apps) {
     index += 1;
+    const PATH = `./${CACHE_DIR}/${index
+      .toString()
+      .padStart(4, "0")}.${appID}.json`;
+
+    paths.push(PATH);
+
+    // 1. Caching -> check if file already exist.
+    const exists = await fileExists(PATH);
+    if (exists) {
+      console.info(
+        `${index}: Cache hit! --> https://github.com/flathub/${appID}`
+      );
+      continue;
+    }
 
     // 2. Query for the history of the metafile belonging to the appID
     const metafile = await queryMetafileHistory(appID);
-    if (!metafile) {
-      // no history found
-      console.log(
-        `${index}: https://github.com/flathub/${appID} -------> No history found`
+    if (!metafile || metafile.history.length === 0) {
+      console.info(
+        `${index}: No history found for --> https://github.com/flathub/${appID}`
       );
       continue;
     }
 
     // 3. Log a line, to see progress when running the tool.
-    console.log(`${index}: Status ${metafile.status}: ${metafile.displayURL}`);
+    console.info(`${index}: Status ${metafile.status}: ${metafile.displayURL}`);
 
-    // 4. Append metafile-object to file
-    const notTheLastApp = index !== apps.length
-    await appendFile(MEATFILES_JSON, `${JSON.stringify(metafile, null, 2)}${notTheLastApp ? "," : ""}`);
+    // 4. Serialize metafile-object to file
+    await writeFile(PATH, JSON.stringify(metafile, null, 2));
+  }
+
+  return paths;
+}
+
+/** Make all metafiles/ into one giant metafiles.json */
+async function combineFiles(paths, outpath) {
+  let output = `[`;
+  for (let i = 0; i < paths.length; ++i) {
+    const path = paths[i];
+    const json = await readFile(path);
+    output += json;
+
+    if (i < paths.length - 1) {
+      output += ",";
+    } else {
+      output += "\n]";
+    }
+  }
+
+  await writeFile(outpath, output);
+}
+
+async function fileExists(path) {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
   }
 }
